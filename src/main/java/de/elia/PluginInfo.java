7e7baa66a4dee9227f5107bd777820e68c249b04
Elia1802 <timlucas.huth@outlook.de>
2023-12-16 21:07:43 +0100

    Edit an info about SoulLibrary-3.0.1.jar

--- src/main/java/de/elia/PluginInfo.java
+++ src/main/java/de/elia/PluginInfo.java
@@ -5,9 +5,14 @@ import java.lang.String;
 public class PluginInfo {
 
   public static final String NAME = "SoulBossSystem";
-  public static final String VERSION = "2.0.0";
+  public static final String VERSION = "1.0.0-BETA";
   public static final String API_NAME = "PaperMC Dev-Bundle";
   public static final String API_VERSION = "1.20.4-R0.1-SNAPSHOT";
   public static final String AUTHOR = "Elia, D1p4k, Zopnote, Sinan";
+
+  public static class SoulLibrary {
+    public static final String NAME = "SoulLibrary";
+    public static final String VERSION = "3.0.1";
+  }
 
 }
